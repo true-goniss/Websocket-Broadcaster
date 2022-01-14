d378454e3a3e1dd6867f6e7973298cef47ac45cb
true-goniss <goniss9801@gmail.com>
2022-01-14 23:15:13 +0300

    Добавьте файлы проекта.

---
+++ WebsocketBroadcaster/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WebsocketBroadcaster
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelPort = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelPort
+            // 
+            this.labelPort.AutoSize = true;
+            this.labelPort.Location = new System.Drawing.Point(27, 4);
+            this.labelPort.Name = "labelPort";
+            this.labelPort.Size = new System.Drawing.Size(37, 13);
+            this.labelPort.TabIndex = 0;
+            this.labelPort.Text = "12000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 22);
+            this.Controls.Add(this.labelPort);
+            this.Name = "Form1";
+            this.Text = "WebsocketBroadcaster";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelPort;
+    }
+}
+
---
+++ WebsocketBroadcaster/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace WebsocketBroadcaster
+{
+    public partial class Form1 : Form
+    {
+
+        WebSocketServer wss;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            string port = "12000";
+            string wssUrl = "ws://" + System.Net.IPAddress.Any + ":" + port;
+            labelPort.Text = port;
+
+            wss = new WebSocketServer(wssUrl);
+            wss.Start();
+            wss.AddWebSocketService<SocketBehavior>("/");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class SocketBehavior : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Sessions.Broadcast(e.Data);
+        }
+
+        protected override void OnOpen()
+        {
+
+        }
+    }
+}
